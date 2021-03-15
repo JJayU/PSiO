9fbb383f45465ace5c043b87c842b39442a8db2b
JJayU <50557966+JJayU@users.noreply.github.com>
2021-03-15 21:59:44 +0100

    Lab2 Z1

---
+++ Lab 2/PSiO Lab2/PSiO Lab2/Z1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+bool is_palindrome(std::string input)
+{
+    std::string input_fixed = "";
+    std::string flipped = "";
+
+    for (size_t i = 0; i < input.length(); i++)
+    {
+        while (input[i] == ' ')
+        {
+            i++;
+        }
+        input_fixed += std::tolower(input[i]);
+    }
+
+    for (size_t i = input_fixed.length() - 1; i > 0; i--)
+    {
+        flipped += input_fixed[i];
+    }
+    flipped += input_fixed[0];
+
+    if (input_fixed == flipped)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    std::string slowo;
+
+    std::cout << "Podaj wyrazenie: ";
+    std::getline(std::cin, slowo);
+
+    if (is_palindrome(slowo))
+    {
+        std::cout << "Wyrazenie \"" << slowo << "\" jest palindromem\n";
+    }
+    else
+    {
+        std::cout << "Wyrazenie \"" << slowo << "\" nie jest palindromem\n";
+    }
+}
